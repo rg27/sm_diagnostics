46d8a0d2c00f226fcf72dbdc123daf0d803a480d
Macbook Pro 15' i7 <gualvezreymar@gmail.com>
2021-10-28 05:22:08 +0800

    Initial Commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,27 +1,33 @@
 export const Header = (props) => {
   return (
     <header id='header'>
-      <div className='intro'>
-        <div className='overlay'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-md-8 col-md-offset-2 intro-text'>
-                <h1>
-                  {props.data ? props.data.title : 'Loading'}
-                  <span></span>
-                </h1>
-                <p>{props.data ? props.data.paragraph : 'Loading'}</p>
-                <a
-                  href='#features'
-                  className='btn btn-custom btn-lg page-scroll'
-                >
-                  Learn More
-                </a>{' '}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+		<div className='intro'>
+			<div className='overlay'>
+				<div className='container'>
+					<div className='row'>
+						<div className='col-md-8 col-md-offset-2 intro-text'>
+							<h1>
+							{props.data ? props.data.title : 'Loading'}
+							<span></span>
+							</h1>
+							<p>{props.data ? props.data.paragraph : 'Loading'}</p>
+							<a
+							href='#features'
+							className='btn btn-custom btn-lg page-scroll'
+							>
+							*NEW* Coronavirus RT-PCR TestinFg
+							</a>{' '}
+							<a
+							href='#features'
+							className='btn btn-custom btn-lg page-scroll'
+							>
+							Login/View Results
+							</a>{' '}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
     </header>
   )
 }
