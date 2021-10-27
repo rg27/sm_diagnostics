071f4ed3ad538c39d193e2b09fe929dfb64ca727
Macbook Pro 15' i7 <gualvezreymar@gmail.com>
2021-10-28 06:01:45 +0800

    Removing and adding components

---
+++ src/components/faqs.jsx
@@ -0,0 +1,18 @@
+export const Faqs = (props) => {
+  return (
+    <div id='faqs' className='text-center'>
+        <div className='container'>
+            <div className='col-md-10 col-md-offset-1 section-title'>
+                <h2>Faqs</h2>
+            </div>
+
+            <div className='row'>
+                <div className='portfolio-items col-xs-6 col-md-3'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu sollicitudin lorem. Fusce vel quam nec odio auctor interdum sit amet id neque. Maecenas dictum, arcu eu tincidunt tempus, nunc lorem convallis massa, sed luctus eros augue congue odio. Aenean dolor ante, gravida sit amet tellus non, vehicula maximus velit. Suspendisse mattis elementum turpis, in vestibulum mi pharetra sit amet. Mauris tincidunt luctus ultrices. Nunc porttitor facilisis purus, ut scelerisque neque congue id. Pellentesque pretium fringilla dui, vitae tristique felis hendrerit vel. Morbi nisl dui, malesuada ac pretium quis, fermentum sollicitudin ipsum. Fusce eget lectus massa. Vestibulum mollis lobortis lectus eu tincidunt.
+                </div>
+            </div>
+        </div>
+    </div>
+    
+  )
+}
