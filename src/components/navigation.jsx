4440d8581fc859231d0844a46874e3ad4b56e477
Macbook Pro 15' i7 <gualvezreymar@gmail.com>
2021-11-04 02:06:49 +0800

    Polishing the UI

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,5 +1,5 @@
 export const Navigation = (props) => {
-  return (
+    return (
         <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
             <div className='container'>
                 <div className='navbar-header'>
@@ -23,12 +23,6 @@ export const Navigation = (props) => {
 
                 <div className='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
                     <ul className='nav navbar-nav navbar-right'>
-                        {/* <li>
-                            <a href='#features' className='page-scroll'>
-                                Features
-                            </a>
-                        </li> */}
-
                         <li>
                             <a href='#about' className='page-scroll'>
                                 About
@@ -47,27 +41,15 @@ export const Navigation = (props) => {
                             </a>
                         </li>
 
-                        {/* <li>
-                            <a href='#portfolio' className='page-scroll'>
-                                Gallery
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href='#testimonials' className='page-scroll'>
-                                Testimonials
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href='#team' className='page-scroll'>
-                                Team
-                            </a>
-                        </li> */}
-
                         <li>
                             <a href='#contact' className='page-scroll'>
                                 Contact
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href='tel:7864221679' className='page-scroll'>
+                               <i class="fa fa-mobile"></i> <strong class ="strong">(786)422-1679</strong>
                             </a>
                         </li>
                     </ul>
